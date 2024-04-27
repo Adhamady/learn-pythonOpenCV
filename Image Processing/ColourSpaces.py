71f09ccbb80192c9eb3d19d6d2539e2c7c7f95c2
Adhamady <Adhamamrkamalmady@gmail.com>
2024-04-27 04:02:31 +0300

    Added colourSpaces.py & Histogram.py

---
+++ Image Processing/ColourSpaces.py
@@ -0,0 +1,20 @@
+# import opencv 
+import cv2 
+
+# Load the input image 
+img = cv2.imread("Images\Innova.jpg")
+cv2.imshow('Original', img) 
+
+
+# Use the cvtColor() function to grayscale the image 
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+cv2.imshow('Grayscale', gray_image) 
+
+hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
+cv2.imshow('HSV', hsv_image) 
+
+# Window shown waits for any key pressing event
+cv2.waitKey(0) 
+
+# Window shown waits for any key pressing event 
+cv2.destroyAllWindows()
