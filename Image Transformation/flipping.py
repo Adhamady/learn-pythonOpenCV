566fad1332bcfd5673756b61d789bf45c8a195f3
Adhamady <Adhamamrkamalmady@gmail.com>
2024-04-27 17:00:48 +0300

    Modified folder structure and added more examples

--- Image Transformation/flipping.py
+++ Image Transformation/flipping.py
@@ -3,3 +3,13 @@ import imutils
 
 img = cv2.imread("Images/Innova.jpg")
 cv2.imshow("original", img)
+
+flipped = cv2.flip(img, 1)
+cv2.imshow("Flipped Horizontally", flipped)
+
+flipped = cv2.flip(img, 0)
+cv2.imshow("Flipped Vertically", flipped)
+
+flipped = cv2.flip(img, -1)
+cv2.imshow("Flipped Horizontally & Vertically", flipped)
+cv2.waitKey(0)
